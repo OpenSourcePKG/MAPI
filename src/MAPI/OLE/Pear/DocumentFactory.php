08afbca35fbe2d133788c3309136627eff8995e1
Stefan Werfling <stefan.werfling@pegenau.de>
2023-11-07 09:46:23 +0100

    fix OLE root null, throw an exception

--- src/MAPI/OLE/Pear/DocumentFactory.php
+++ src/MAPI/OLE/Pear/DocumentFactory.php
@@ -6,16 +6,31 @@ use Hfig\MAPI\OLE\CompoundDocumentElement;
 use Hfig\MAPI\OLE\CompoundDocumentFactory;
 use OLE;
 
+/**
+ * MAPI Document factory, result by a source the CompoundDocumentElement.
+ */
 class DocumentFactory implements CompoundDocumentFactory
 {
+    /**
+     * @param string $file
+     * @throws \Exception
+     */
     public function createFromFile($file): CompoundDocumentElement
     {
         $ole = new OLE();
         $ole->read($file);
 
+        if ($ole->root === null) {
+            throw new \Exception('MAPI->DocumentFactory->createFromFile->OLE->root === null, Can not trasform File to DocumentElement: ' . $file);
+        }
+
         return new DocumentElement($ole, $ole->root);
     }
 
+    /**
+     * @param string $stream
+     * @throws \Exception
+     */
     public function createFromStream($stream): CompoundDocumentElement
     {
         // PHP buffering appears to prevent us using this wrapper - sometimes fseek() is not called
@@ -23,6 +38,10 @@ class DocumentFactory implements CompoundDocumentFactory
 
         $ole = new OLE();
         $ole->readStream($stream);
+
+        if ($ole->root === null) {
+            throw new \Exception('MAPI->DocumentFactory->createFromStream->OLE->root === null, Can not trasform stream to DocumentElement.');
+        }
 
         return new DocumentElement($ole, $ole->root);
     }
